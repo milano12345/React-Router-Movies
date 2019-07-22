0763890cc8d5d2cf6b873315c655ab6bc3b4e9d8
milano12345 <dmilano857@gmail.com>
2019-07-22 14:28:40 -0600

    fixed errors and added functionalility to cards

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -9,9 +9,9 @@ export default class Movie extends Component {
     };
   }
 
-  componentDidMount() {
-    // change this line to grab the id passed on the URL
-    const id = 1;
+  componentDidMount(props) {
+ 
+    const id = this.props.match.params.id;
     this.fetchMovie(id);
   }
 
